b4935ccc2e02e964dfc77ab6d7b19340001c38da
Nairon <saske11@mail.ru>
2021-01-27 11:17:55 +0400

    feat add user authorizations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,22 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, excep:[:index, :show]
+  before_action :authenticate_user!, except:[:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.order(created_at: :desc)
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+
   def create
     @post = Post.new(post_params)
 
@@ -38,9 +31,8 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
+    authorize! :manage, @post
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -55,6 +47,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    authorize! :manage, @post
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
